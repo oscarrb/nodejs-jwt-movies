d84d963db68ca1ea783e8000689960b7318bcb4c
oscar.rodriguez <oscar.rodriguez@mimacom.com>
2020-06-26 19:08:57 +0200

    Minor change

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken')
 
 const authMiddleware = (req, res, next) => {
     // read the token from header or url 
-    const token = req.headers['x-access-token'] || req.headers['authorization'].split(" ")[1] || req.query.token
+    const token = req.headers['x-access-token'] || req.headers['Authorization'].split(" ")[1] || req.query.token
 
     // token does not exist
     if(!token) {
