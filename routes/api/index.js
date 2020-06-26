4d13da078c66e38d5da632828a028c33672bd0d0
oscar.rodriguez <oscar.rodriguez@mimacom.com>
2020-06-26 12:47:17 +0200

    Improve index.js middleware usage

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,4 +1,3 @@
-const app = require('express');
 const router = require('express').Router()
 const authMiddleware = require('../../middlewares/auth')
 const corsMiddleware = require('../../middlewares/cors')
@@ -6,7 +5,7 @@ const auth = require('./auth')
 const user = require('./user')
 const favourite = require('./favourite')
 
-app.use(corsMiddleware);
+router.use(corsMiddleware);
 
 router.use('/auth', auth)
 router.use('/user', authMiddleware)
